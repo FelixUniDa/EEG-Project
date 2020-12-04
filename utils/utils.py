779546b87445691e9d3de53d1594921223bd763a
Felix <felix.wirth@stud.tu-darmstadt.de>
2020-12-04 15:15:09 +0100

    SRC-3 utils
    skeleton for utils

---
+++ utils/utils.py
@@ -0,0 +1,60 @@
+import numpy as np
+
+
+def create_signal(arg1, arg2):
+    """
+    short description of function
+    :param arg1:
+    :param arg2:
+    :return:
+    """
+
+def create_noise(arg1, arg2):
+    """
+
+    :param arg1:
+    :param arg2:
+    :return:
+    """
+
+
+def create_outlier(arg1, arg2):
+    """
+
+    :param arg1:
+    :param arg2:
+    :return:
+    """
+
+
+def covariance(arg1, arg2):
+    """
+
+    :param arg1:
+    :param arg2:
+    :return:
+    """
+
+def mixing_matrix(arg1, arg2):
+    """
+
+    :param arg1:
+    :param arg2:
+    :return:
+    """
+
+def whitening(arg1, arg2):
+    """
+
+    :param arg1:
+    :param arg2:
+    :return:
+    """
+
+def centering(arg1, arg2):
+    """
+
+    :param arg1:
+    :param arg2:
+    :return:
+    """
