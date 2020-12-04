e65fe96ddb2bd6a8f9e5ae20cee686a10a67f826
Felix <felix.wirth@stud.tu-darmstadt.de>
2020-12-04 14:48:42 +0100

    SRC-11 MD
    initial document for different distance measures

---
+++ utils/distances.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+############################ v0.01 #######################
+"""
+util function for different distance measure
+"""
+
+
+def md(arg1, arg2):
+    """
+    Minimum Distance
+    :param arg1:
+    :param arg2:
+    :return:
+    """
