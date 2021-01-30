85cf81f956e123ea39a85cafaedeca9a081ac621
Taulant Koka <koka.taulant@outlook.de>
2021-01-31 00:11:36 +0100

    Test File Graph Signals Update

--- Python_Code/Graph_PowerICA/graphICA_test.py
+++ Python_Code/Graph_PowerICA/graphICA_test.py
@@ -8,27 +8,33 @@ from PowerICA import*
 
 if __name__ == "__main__":
 
+    N = 1000
+    P = 4
+    ### Set up experiment as in R Code ###
+    ### Erdos Renyi Graph ###
+    G1 = graphs.ErdosRenyi(N,p=0.05).W.toarray()
+    G2 = G1 @ G1
+    Ws = np.array([G1,G1,G1,G1,G2,G2,G2,G2]).reshape(1000,1000,8)
 
-    data = np.stack([create_signal(f=2,c='ecg',x=1000),
-                    create_signal(f=5,c='cos',x=1000),
-                    create_signal(f=10,c='rect',x=1000), 
-                    create_signal(f=25,c='sawt',x=1000)]).T
-    
-    N = data.shape[0]
+    ### Graph Moving Average Signals ###
+    e = np.zeros((N,P))
+    e[:,0] = np.random.standard_t(5,N)
+    e[:,1] = np.random.standard_t(10,N)
+    e[:,2] = np.random.standard_t(15,N)
+    e[:,3] = np.random.randn(N)
 
-    G1 = graphs.ErdosRenyi(N,p=0.5)
-    G2 = graphs.ErdosRenyi(N,p=0.3)
-    G3 = graphs.ErdosRenyi(N,p=0.2)
-    G4 = graphs.ErdosRenyi(N,p=0.15)
-    Ws = np.array([G1.W.toarray(),G1.W.toarray(),G1.W.toarray(),G1.W.toarray()]).reshape(1000,1000,4)
+    theta = np.array([0.02,0.04,0.06,0.08])
+    S = np.zeros((N, P))
+    for j in range(0,P):
+      S[:,j] = e[:,j]+theta[j]*Ws[:,:,j]@e[:,j]   
 
-
+    ### Mix Signals and whiten ###
     mixmat = mixing_matrix(4)
-    mixeddata = mixmat @ data.T
-    mixdata_noise = np.stack([create_outlier(apply_noise(dat,type='white', SNR_dB=10),prop=0.00,type='impulse',std=100) for dat in mixeddata])
-    white_data,W_whiten,W_dewhiten,_ = whitening(mixdata_noise, type='sample')
+    mixeddata = mixmat @ S.T
+    #mixdata_noise = np.stack([create_outlier(apply_noise(dat,type='white', SNR_dB=10),prop=0.00,type='impulse',std=100) for dat in mixeddata])
+    white_data,W_whiten,W_dewhiten,_ = whitening(mixeddata, type='sample')
 
-    W_graphpower,_ = Graph_powerICA(white_data,'pow3',Ws,0.4)
+    W_graphpower,_ = Graph_powerICA(white_data,'pow3',Ws,b=1)
     W_power,_ = powerICA(white_data,'pow3')
 
     unMixed_graphpower = W_graphpower @ white_data
@@ -36,7 +42,9 @@ if __name__ == "__main__":
 
     md_powerica = md(W_whiten@mixmat,W_power)
     md_graphpowerica = md(W_whiten@mixmat,W_graphpower)
+    print(md_powerica, md_graphpowerica)
 
+    ### plot figures ###
     fig1, axs1 = plt.subplots(4, sharex=True)
     plt.xlim(xmin=0, xmax=1000 )
     fig2, axs2 = plt.subplots(4, sharex=True)
@@ -48,10 +56,9 @@ if __name__ == "__main__":
     fig5, axs5 = plt.subplots(4, sharex=True)
     plt.xlim(xmin=0, xmax=1000)
     i=0
-    print(md_powerica, md_graphpowerica)
     while(i<4):
         # input signals
-        axs1[i].plot(data[:, i], lw=3)
+        axs1[i].plot(S[:, i], lw=3)
         axs1[i].set_ylabel('sig: ' + str(i))
         fig1.suptitle('Input Signals')
 
@@ -59,9 +66,9 @@ if __name__ == "__main__":
         axs2[i].set_ylabel('sig: ' + str(i))
         fig2.suptitle('Mixed Signals')
 
-        axs3[i].plot(mixdata_noise.T[:, i], lw=3)
-        axs3[i].set_ylabel('sig: ' + str(i))
-        fig3.suptitle('Contaminated Mixed Signals')
+        # axs3[i].plot(mixdata_noise.T[:, i], lw=3)
+        # axs3[i].set_ylabel('sig: ' + str(i))
+        # fig3.suptitle('Contaminated Mixed Signals')
 
         axs4[i].plot((unMixed_graphpower).T[:, i], lw=3)
         axs4[i].set_ylabel('sig: ' + str(i))
